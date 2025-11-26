90eab5e728a0d83f52cbc66ef46273a62f993fda
paivac2atwit <paivac2@wit.edu>
2025-11-25 21:00:33 -0500

    Assignment 6

--- parta.c
+++ parta.c
@@ -3,46 +3,110 @@
 #include <stdio.h>
 
 /**
- * TODO: Describe what the function does
+ * Takes an array of CPU bursts, and returns an array of PCBs.
  */
-struct pcb* init_procs(int* bursts, int blen) {
-    return NULL;
+ struct pcb* init_procs(int* bursts, int blen) {
+    struct pcb* procs = malloc(sizeof(struct pcb) * blen);
+    if(procs == NULL) 
+        return NULL;
+    
+    for (int i = 0; i < blen; i++) {
+        procs[i].pid = i;
+        procs[i].burst_left = bursts[i];
+        procs[i].wait = 0;
+    }
+return procs;
 }
-
-/**
- * TODO: Describe what the function does
+/*
+ * Prints the current values of each PCB.
  */
 void printall(struct pcb* procs, int plen) {
-    // Feel free to print however you want
+printf("Current process states:\n");
+    for (int i = 0; i < plen; i++) {
+        printf("PID %d: burst_left=%d, wait=%d\n",
+               procs[i].pid,
+               procs[i].burst_left,
+               procs[i].wait);
+    }
+    printf("\n");
 }
 
 /**
- * TODO: Describe what the function does
+ * Reduces the selected process’s burst_left by amount, and increases the wait time of every other process by the same amount.
  */
 void run_proc(struct pcb* procs, int plen, int current, int amount) {
-    // TODO: Complete and document
+    if (procs[current].burst_left > 0) {
+        procs[current].burst_left -= amount;
+        if (procs[current].burst_left < 0) {
+            procs[current].burst_left = 0;
+        }
+    }
+    procs[current].wait =0;
+    for (int i = 0; i < plen; i++) {
+        if (i == current) continue;
+        if (procs[i].burst_left > 0) {
+            procs[i].wait += amount;
+        }
+    }
 }
 
 /**
- * TODO: Describe what the function does
+ * Simulates a First-Come, First-Served (FCFS) CPU scheduling algorithm over an array of PCBs.
  */
 int fcfs_run(struct pcb* procs, int plen) {
-    // TODO: Complete and document
-    return -1;
+    int total_time = 0;
+
+    for (int i = 0; i < plen; i++) {
+        procs[i].wait = total_time;
+        total_time =+ procs[i].burst_left;
+        procs[i].burst_left = 0;
+    }
+    return total_time;
 }
 
 /**
- * TODO: Describe what the function does
+ * Determines the next runnable process in the Round Robin queue.
  */
 int rr_next(int current, struct pcb* procs, int plen) {
-    // TODO: Complete and document
+    int all_done = 1;
+    for (int i = 0; i < plen; i++) {
+        if (procs[i].burst_left > 0) {
+            all_done = 0;
+            break;
+        }
+    }
+    if (all_done) {
+    return -1;
+    } 
+
+    int next = (current + 1) % plen;
+
+    for (int i = 0; i < plen; i++) {
+        int idx = (next + i) % plen;
+        if (procs[idx].burst_left > 0) {
+            return idx;
+        }
+    }
+
     return -1;
 }
 /**
- * TODO: Describe what the function does
+ * Runs all processes using Round Robin scheduling with the given time quantum and returns the total execution time.
  */
 int rr_run(struct pcb* procs, int plen, int quantum) {
-    // TODO: Complete and document
-    return -1;
+    int total_time = 0;
+    int current = 0;
+
+    while (1) {
+        current = rr_next(current, procs, plen);
+        if (current == -1) 
+            break;
+
+        int run_time = (procs[current].burst_left < quantum) ? procs[current].burst_left : quantum;
+        
+        run_proc(procs, plen, current, run_time);
+        total_time += run_time;
+    }
+    return total_time;
 }
 
--- parta_main.c
+++ parta_main.c
@@ -5,6 +5,86 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+    if (argv < 2) {
+        printf("Error: Missing arguements\n");
+        return 1;
+    }
+
+    char* algo = argv[1];
+
+    if (strcmp(algo, "fcfs") == 0) {
+        if (argc < 3) {
+            printf("Error: Missing arguements\n");
+            return 1;
+        }
+    int plen = argc - 2;
+    int* bursts = malloc(sizeof(int) * plen);
+    for (int i = 0; i < plen; i++) {
+        bursts[i] = atoi(argv[i + 2]);
+    }
+
+    struct pcb* procs = init_procs(bursts, plen);
+    free(bursts);
+
+    printf("Using FCFS\n\n");
+
+    for (int i = 0; i < plen; i ++){
+        printf("Accepted P%d: Burst %d\n", procs[i].pid, procs[i].burst_left);
+
+    }
+
+    int total_time = fcfs_run(procs, plen);
+
+    int sum_wait = 0;
+    for (int i = 0; i < plen; i++) {
+        sum_wait += procs[i].wait;
+    }
+
+    double avg_wait = (double)sum_wait / plen;
+
+    printf("Average wait time: %2f\n", avg_wait);
+
+    free(procs);
     return 0;
+    }
+    else if (strcmp(algo, "rr") == 0) {
+        if (argc < 4) {
+            printf("Error: Missing arguemtns\n");
+            return 1;
+        }
+        int quantum = atoi(argv[2]);
+        int plen = argc - 3;
+        int* bursts = malloc(sizeof(int) * plen);
+        for (int i =0; i < plen; i++) {
+            bursts[i] = atoi(argv[i + 3]);
+        }
+
+        struct pcb* procs = init_procs(bursts, plen);
+        free(bursts);
+
+        printf("Using RR(%d)\n\n", quantum);
+
+        for (int i = 0; i < plen; i++) {
+            printf("Accepted P%d: Burst %d\n", procs[i].pid, procs[i].burst_left);
+        }
+
+        int total_time = rr_run(procs, plen, quantum);
+
+        int sum_wait = 0;
+        for (int i =0; i < plen; i++) {
+            sum_wait += procs[i].wait;
+        }
+
+        double avg_wait = (double)sum_wait / plen;
+
+        printf("Average wait time: %.2f\n", avg_wait);
+
+        free(procs);
+        return 0;
+    }
+    
+    else {
+        printf("Error: Missing arguements\n");
+        return 1;
+    }
 }
